01d43e3a8f43fd8813ee9278cc0cdccc65b408a1
oscarracuna <acunainfosec@proton.me>
2024-12-25 14:59:26 -0700

    2

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	godotenv.Load()
+  api_key := os.Getenv("API_KEY")
+  city := "Ciudad Juarez"
+  url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s", api_key, city)
+  resp, err := http.Get(url)
+  if err != nil {
+    log.Print("Something went wrong fetching data")
+    return
+  } else {
+    fmt.Println(resp)
+  }
+  defer resp.Body.Close()
 }
